e8e31fd2514544899373780a41d4cfb305cce49c
Mike Malcolm <mike.j.malcolm@gmail.com>
2024-02-22 14:46:10 +0000

    changes listening message and added new endpoint to get a pokemon (#2)

--- app/server.js
+++ app/server.js
@@ -8,6 +8,17 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get('/pokemon/:id', (req, res) => {
+    res.send({
+        id: 6,
+        name: 'Charizard',
+        type: ['Fire', 'Flying'],
+        height_m: 1.7,
+        weight_kg: 90.5,
+        weakness: ['Water', 'Electric', 'Rock']
+    })
+});
+
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+    console.log(`Listening on http://localhost:${PORT}`);
 });
